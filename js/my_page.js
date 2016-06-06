7971e4b96db814f655a1709d5b0587c8ee87cb0c
Pashebor <pashebor@gmail.com>
2016-06-06 17:24:58 +0400

    Some fixes

--- js/my_page.js
+++ js/my_page.js
@@ -32,6 +32,7 @@ $(window).on('load', function () {
     var container = $('.container');
     var circle = $('.circle');
     var flask = $('#flask');
+    var wave = $('.wave');
     flask.hide();
     circle.css({
         'animation' : 'circleAnimationIn',
@@ -39,14 +40,27 @@ $(window).on('load', function () {
         'animation-iteration-count' : '1',
         'width' : '50px',
         'height' : '50px',
-        'margin-left' : 'calc(50% - 25px)',
+        /*'margin-left' : 'calc(50% - 25px)',*/
         'top': '40%',
         'border-radius' : '25px',
         'cursor' : 'pointer'
     });
-
        setTimeout(function () {
-           flask.fadeIn('slow')
+          circle.css('margin-left', 'calc(50% - 25px)');
+       }, 1000);
+       setTimeout(function () {
+           flask.fadeIn('slow');
+           circle.hover(function () {
+              wave.css({
+                  'animation' : 'waves',
+                  'animation-duration' : '7s',
+                  'animation-iteration-count' : '1'
+              });
+           },
+           function () {
+               wave.css('animation', 'none');
+           }
+           );
        }, 1000);
     container.hide();
     changeFlask();
@@ -56,12 +70,13 @@ $(document).ready( function() {
  var flask = $('#flask');
  var circle = $('.circle');
  var container = $('.container');
+ var wave = $('.wave');
    circle.click(function () {
        flask.fadeOut('slow');
        setTimeout( function () {
        circle.css({
           'animation' : 'circleAnimationOut',
-           'animation-duration' : '2s',
+           'animation-duration' : '1s',
            'animation-iteration-count' : '1',
            'width' : '100%',
            'height' : '100%',
@@ -69,6 +84,7 @@ $(document).ready( function() {
            'top': '0',
            'border-radius' : '0'
        });
+       wave.css('animation', 'none');
        container.fadeIn('slow').css({
            'position': 'absolute'
        });
